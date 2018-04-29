7ae862ae4ce45fd251e8ae6f9c54767c6dabfc8a
Mohamed Shahin <M7edshin@hotmail.com>
2018-04-29 20:55:55 +0100

    Fixed untracked Files

--- app/src/main/java/e/m7eds/simpleexampleexoplayer/MainActivity.java
+++ app/src/main/java/e/m7eds/simpleexampleexoplayer/MainActivity.java
@@ -3,10 +3,14 @@ package e.m7eds.simpleexampleexoplayer;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
@@ -15,6 +19,7 @@ import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
@@ -24,17 +29,27 @@ import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
 
 public class MainActivity extends AppCompatActivity implements ExoPlayer.EventListener{
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    public  final String PLAYER_PLAYBACK_POSITION_STATE_KEY = "player_playback_position_state_key";
+    public  final String PLAYER_CURRENT_WINDOW_STATE_KEY = "player_current_window_state_key";
+    public  final String PLAYER_WHEN_READY_STATE_KEY = "player_when_ready_state_key";
 
-    SimpleExoPlayer exoPlayer;
-    SimpleExoPlayerView exoPlayerView;
-    PlaybackStateCompat.Builder stateBuilder;
-    MediaSessionCompat mediaSession;
+
+    private SimpleExoPlayerView exoPlayerView;
+    private SimpleExoPlayer exoPlayer;
+    private String videoUrl = "asset:///video.mp4";
+    private boolean playWhenReady;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
+    private PlaybackStateCompat.Builder stateBuilder;
+    private MediaSessionCompat mediaSession;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,37 +58,117 @@ public class MainActivity extends AppCompatActivity implements ExoPlayer.EventLi
 
         exoPlayerView = findViewById(R.id.exo_player_view);
 
+        if(savedInstanceState!=null){
+            playbackPosition = savedInstanceState.getLong(PLAYER_PLAYBACK_POSITION_STATE_KEY);
+            playWhenReady = savedInstanceState.getBoolean(PLAYER_WHEN_READY_STATE_KEY);
+            currentWindow = savedInstanceState.getInt(PLAYER_CURRENT_WINDOW_STATE_KEY);
+        }
+
         initializeMediaSession();
-        playMedia();
+        initializePlayer();
 
     }
 
-    private void playMedia(){
-        exoPlayerView.setDefaultArtwork(BitmapFactory.decodeResource
-                (getResources(), R.drawable.image));
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
 
-        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(null);
-        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+        Log.v("Tag", "MyLogs: onSaveInstanceState Method is launched");
 
-        exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
-        exoPlayerView.requestFocus();
-        exoPlayerView.setUseController(true);
-        exoPlayerView.setPlayer(exoPlayer);
+        playbackPosition = exoPlayer.getCurrentPosition();
+        playWhenReady = exoPlayer.getPlayWhenReady();
+        currentWindow = exoPlayer.getCurrentWindowIndex();
 
-        String userAgent = Util.getUserAgent(this, "ExampleExoPlayer");
-        DataSource.Factory dataSource = new DefaultDataSourceFactory(this, userAgent);
-        //You can either provide path of audio or video as example:
-        /*
-        Uri file = Uri.parse("asset:///audio.mp3");
-        Uri file = Uri.parse("asset:///video.mp4");
-        */
-        Uri file = Uri.parse("asset:///video.mp4");
+        outState.putLong(PLAYER_PLAYBACK_POSITION_STATE_KEY, playbackPosition);
+        outState.putBoolean(PLAYER_WHEN_READY_STATE_KEY,playWhenReady);
+        outState.putInt(PLAYER_CURRENT_WINDOW_STATE_KEY,currentWindow);
+    }
 
-        ExtractorMediaSource extractorMediaSource = new ExtractorMediaSource(file, dataSource,
-                new DefaultExtractorsFactory() ,null, null);
-        exoPlayer.prepare(extractorMediaSource);
-        exoPlayer.setPlayWhenReady(true);
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if(savedInstanceState!=null){
+            playbackPosition = savedInstanceState.getLong(PLAYER_PLAYBACK_POSITION_STATE_KEY);
+            playWhenReady = savedInstanceState.getBoolean(PLAYER_WHEN_READY_STATE_KEY);
+            currentWindow = savedInstanceState.getInt(PLAYER_CURRENT_WINDOW_STATE_KEY);
+        }
+    }
 
+    private void initializePlayer() {
+        if(exoPlayer==null){
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(
+                    new DefaultRenderersFactory(this),
+                    new DefaultTrackSelector(), new DefaultLoadControl());
+
+            exoPlayerView.setPlayer(exoPlayer);
+
+            exoPlayer.setPlayWhenReady(playWhenReady);
+            exoPlayer.seekTo(currentWindow, playbackPosition);
+        }
+
+        Uri uri = Uri.parse(videoUrl);
+        MediaSource mediaSource = buildMediaSource(uri);
+        exoPlayer.prepare(mediaSource, true, false);
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource.Factory(
+                new DefaultHttpDataSourceFactory("StepDetailsFragment")).
+                createMediaSource(uri);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.v("Tag", "MyLogs: OnStart Method is launched");
+
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.v("Tag", "MyLogs: OnResume Method is launched");
+
+        if ((Util.SDK_INT <= 23 || exoPlayer == null)) {
+            initializePlayer();
+        }
+    }
+
+    private void releasePlayer() {
+        if (exoPlayer != null) {
+            playbackPosition = exoPlayer.getCurrentPosition();
+            currentWindow = exoPlayer.getCurrentWindowIndex();
+            playWhenReady = exoPlayer.getPlayWhenReady();
+            exoPlayer.stop();
+            exoPlayer.release();
+            exoPlayer = null;
+        }
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            Log.e("Tag", "MyLogs: OnPause Method is launched");
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        Log.v("Tag", "MyLogs: OnStop Method is launched");
+
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+            Log.v("Tag", "MyLogs: ExoPlayer is released");
+        }
     }
 
     /**
@@ -112,60 +207,6 @@ public class MainActivity extends AppCompatActivity implements ExoPlayer.EventLi
 
     }
 
-    @Override
-    public void onTimelineChanged(Timeline timeline, Object manifest) {
-
-    }
-
-    @Override
-    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-
-    }
-
-    @Override
-    public void onLoadingChanged(boolean isLoading) {
-
-    }
-
-    /**
-     * Method that is called when the ExoPlayer state changes. Used to update the MediaSession and
-     * PlayBackState to keep in sync.
-     * @param playWhenReady true if ExoPlayer is playing, false if it's paused.
-     * @param playbackState int describing the state of ExoPlayer. Can be STATE_READY, STATE_IDLE,
-     *                      STATE_BUFFERING, or STATE_ENDED.
-     */
-    @Override
-    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-        if((playbackState == ExoPlayer.STATE_READY) && playWhenReady){
-            stateBuilder.setState(PlaybackStateCompat.STATE_PLAYING,
-                    exoPlayer.getCurrentPosition(), 1f);
-        } else if((playbackState == ExoPlayer.STATE_READY)){
-            stateBuilder.setState(PlaybackStateCompat.STATE_PAUSED,
-                    exoPlayer.getCurrentPosition(), 1f);
-        }
-        mediaSession.setPlaybackState(stateBuilder.build());
-    }
-
-    @Override
-    public void onRepeatModeChanged(int repeatMode) {
-
-    }
-
-    @Override
-    public void onPlayerError(ExoPlaybackException error) {
-
-    }
-
-    @Override
-    public void onPositionDiscontinuity() {
-
-    }
-
-    @Override
-    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-
-    }
-
     /**
      * Media Session Callbacks, where all external clients control the player.
      */
@@ -186,22 +227,61 @@ public class MainActivity extends AppCompatActivity implements ExoPlayer.EventLi
         }
     }
 
-    /**
-     * Release ExoPlayer.
-     */
-    private void releasePlayer() {
-        exoPlayer.stop();
-        exoPlayer.release();
-        exoPlayer = null;
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
     }
 
-    /**
-     * Release the player when the activity is destroyed.
-     */
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        releasePlayer();
-        mediaSession.setActive(false);
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if((playbackState == ExoPlayer.STATE_READY) && playWhenReady){
+            stateBuilder.setState(PlaybackStateCompat.STATE_PLAYING,
+                    exoPlayer.getCurrentPosition(), 1f);
+        } else if((playbackState == ExoPlayer.STATE_READY)){
+            stateBuilder.setState(PlaybackStateCompat.STATE_PAUSED,
+                    exoPlayer.getCurrentPosition(), 1f);
+        }
+        mediaSession.setPlaybackState(stateBuilder.build());
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
+
     }
 }
